78a3013a8e1698444b1029683f955d1ad9cec4b0
Ibrahim Aksoy <ib.koray@gmail.com>
2018-08-25 18:59:22 +0300

    Implemented google map with api key

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 
 class Map extends Component {
   render() {
+
+    const GoogleMapExample = withGoogleMap(props => (
+      <GoogleMap
+        defaultCenter={{ lat: 41.0439991, lng: 28.9932452 }}
+        defaultZoom={14}
+      >
+      </GoogleMap>
+    ));
+
     return (
-      <div>Map Template</div>
+      <div>
+        <GoogleMapExample
+          containerElement={<div style={{ height: `100vh`, width: '100vw' }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
     );
   }
 };
