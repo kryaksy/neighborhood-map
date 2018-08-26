ab20000490054916dd81892749d94fc07bb6b189
Ibrahim Aksoy <ib.koray@gmail.com>
2018-08-27 00:15:46 +0300

    Deleted logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map.js';
 import * as FourSquareAPI from './FourSquareAPI'
@@ -22,8 +21,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Beşiktaş</h1>
         </header>
         <Map places={this.state.places}/>
       </div>
